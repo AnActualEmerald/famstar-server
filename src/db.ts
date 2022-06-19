fdfd83fb6754d61d9a705a6855a18e8e0deec6a7
AnActualEmerald <emerald_actual@proton.me>
2022-06-19 19:04:56 -0400

    fix heroku postgres

--- src/db.ts
+++ src/db.ts
@@ -3,10 +3,10 @@ import { postgres } from "./deps.ts";
 const dbUrl = Deno.env.get("DATABASE_URL");
 
 if (!dbUrl) {
-    console.error("DATABASE_URL not set");
-    Deno.exit(-1);
+  console.error("DATABASE_URL not set");
+  Deno.exit(-1);
 }
 
-const sql = postgres(dbUrl);
+const sql = postgres(dbUrl, { ssl: "prefer" });
 
 export default sql;
